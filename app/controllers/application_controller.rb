1c60002af6f4f5e742f5dcb71b51324f329fa5ed
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-08-23 16:51:29 -0500

    creating css files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+  private
+    def layout_by_resource
+      devise_controller? ? "session" : "application"
+    end
 end
