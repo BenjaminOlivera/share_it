f2df58fb1eda1aa95ecd4bf1c9c7cbf3b4335be4
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-08-30 16:30:35 -0500

    adding the timelines controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  authenticate :user do
+    resources :timelines,
+      only: [:index, :show]
+  end
 end
