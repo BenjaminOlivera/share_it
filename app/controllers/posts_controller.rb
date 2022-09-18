a30b812320f12f6136d2baa96daa805acf6dc271
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-09-18 15:32:49 -0500

    definiing permited_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def create
+    is_posted = Post::Creator.call(current_user, permitted_params)
+
+    unless is posted
+      flash[:alert] = "Something went wrong"
+    end
+
+    redirect_back fallback_location: timelines_path
+  end
+
+  private
+  def permitted_params
+    params.require(:post).permit(:postable_type, :status_text)
   end
 end
